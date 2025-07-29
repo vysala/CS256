16969dd07b187f0191f5c2ead12df2cba5e19ecd
APV <42871943+vysala@users.noreply.github.com>
2025-07-28 20:40:30 -0700

    Create Escapeseq.java

---
+++ ch01/Escapeseq.java
@@ -0,0 +1,11 @@
+public class Escapeseq {
+    public static void main(String[] args) {
+        System.out.println("Hello!\nHow are you doing?\n");
+        System.out.println("She said \"Hello!\" to me.");
+        System.out.println("\"Hello World,\" I said!");
+        System.out.println("3\t2\t1\tBlastoff!!\nHello Space!");
+        System.out.println("\"Is this symbol \\ a backslash or forward slash?\" I asked.");
+
+
+    }
+}
