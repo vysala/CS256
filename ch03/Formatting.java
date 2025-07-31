ba614ecc5c9df8fdec0f9750be43b412c0ba0639
APV <42871943+vysala@users.noreply.github.com>
2025-07-31 13:03:52 -0700

    Update Formatting.java

--- ch03/Formatting.java
+++ ch03/Formatting.java
@@ -1,6 +1,7 @@
 /**
  * Examples from the middle of the chapter.
  */
+
 public class Formatting {
 
     public static void main(String[] args) {
@@ -10,11 +11,11 @@ public class Formatting {
 
         // formatting output
 
-        System.out.print(4.0 / 3.0);
-        System.out.printf("Four thirds = %.3f", 4.0 / 3.0);
+        System.out.println(4.0 / 3.0);
+        System.out.printf("Four thirds = %.3f\n", 4.0 / 3.0);
 
         int inch = 100;
-        double cm = inch * CM_PER_INCH;
+        double cm = inch * CM_PER_INCH;  // 100 * 2.54= 254.00000 cm
         System.out.printf("%d in = %f cm\n", inch, cm);
 
         // type cast operators
@@ -23,11 +24,12 @@ public class Formatting {
         int xi = (int) pi;
 
         double x = (int) pi * 20.0;  // result is 60.0, not 62.0
+        System.out.println("Value of x is" + x);
 
         inch = (int) (cm / CM_PER_INCH);
         System.out.printf("%f cm = %d in\n", cm, inch);
 
-        System.out.printf("inches = %d" + inch);  // error
+        System.out.printf("inches = %d" + inch);  // error  , since it is printf + needs to be replaced by a , 
     }
 
 }
