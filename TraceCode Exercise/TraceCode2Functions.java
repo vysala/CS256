a014aeb82f0aec08080ea5624f1a61307669948e
APV <42871943+vysala@users.noreply.github.com>
2025-09-03 08:25:40 -0700

    Create TraceCode2Functions.java

---
+++ TraceCode Exercise/TraceCode2Functions.java
@@ -0,0 +1,16 @@
+package FunctionsTopic;
+
+
+public class TraceCode2_Functions {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int result = sumTimesTwo(a, b);
+        System.out.println("Result = " + result);
+    }
+
+    public static int sumTimesTwo(int x, int y) {
+        int s = x + y;
+        return s * 2;
+    }
+}
