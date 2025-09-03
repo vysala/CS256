49e7c98d6da4bfec7e358b8baaafced421fa1f59
APV <42871943+vysala@users.noreply.github.com>
2025-09-03 08:24:42 -0700

    Create TraceCode1.Functions.java

---
+++ TraceCode Exercise/TraceCode1.Functions.java
@@ -0,0 +1,25 @@
+
+
+public class TraceCode1_Functions {
+    
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        
+        int result = multiply(add(x, y), subtract(x, y));
+        
+        System.out.println("Final Result = " + result);
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
