bbd23ca88d6460507a4fac849a397e84483a0a62
APV <42871943+vysala@users.noreply.github.com>
2025-09-07 15:41:58 -0700

    Update NestedifExample.java

--- ConditionalStatements/NestedifExample.java
+++ ConditionalStatements/NestedifExample.java
@@ -1,20 +1,17 @@
-package ConditionalStatements;
+public class NestedIfExample {
+    public static void main(String[] args) {
+        int num = 15;
 
-package ConditionalStatements;
-/* You are using two independent if conditions, and one of them has an else. */
-public class IndependentifExample
-{
-	public static void main(String[] args) {
-		int num=0;
-		if(num>0){
-		    System.out.println("The number is positive");
-		}
-		 if(num%2==0){
-		     System.out.println("The number is divisible by 2");
-		 }
-		  else{
-		      System.out.print("The number is not divisible by 2");
-		   }
-	}
-}
+        if (num > 0) {   // outer if
+            System.out.println("The number is positive");
+
+            if (num % 2 == 0) {   // inner if
+                System.out.println("The number is divisible by 2");
+            } else {
+                System.out.println("The number is not divisible by 2");
+            }
+        } else {
+            System.out.println("The number is not positive");
+        }
+    }
 
