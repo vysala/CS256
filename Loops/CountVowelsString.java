6d6821726dbc06f537da8573bc4f4fb735ba20e6
APV <42871943+vysala@users.noreply.github.com>
2025-09-11 09:32:05 -0700

    Update CountVowelsString.java

--- Loops/CountVowelsString.java
+++ Loops/CountVowelsString.java
@@ -1,58 +1,34 @@
-/*
+//Program: Sum of Digits using Different Loops
+public class SumOfDigitsAllLoops {
+    public static void main(String[] args) {
+        int num = 5678;
 
-Write a program that prompts the user to input a sequence of characters
-and outputs the number of vowels. ( Write a method isVowel that has one parameter.
-
-public static boolean isVowel(char ch)
-{ }  You need to use switch case to check if it is vowel.
- */
-
-
-
- 
-package LoopsDemo;
-
-import java.util.Scanner;
-
-public class CountVowelsString {
-    static Scanner console = new Scanner(System.in);
-
-    public static void main(String[] args)
-    {
-    String inputLine;
-
-    int count = 0;
-
-    System.out.print("Enter characters in a line: ");
-    inputLine = console.nextLine();
-    System.out.println();
-
-    for (int i = 0; i < inputLine.length(); i++)
-        if (isVowel(inputLine.charAt(i)))
-            count++;
-
-    System.out.println();
-    System.out.println("There are " + count + " vowel in the input line");
-}
-
-    public static boolean isVowel(char ch)
-    {
-        switch (ch)
-        {
-        case 'A':
-        case 'E':
-        case 'I':
-        case 'O':
-        case 'U':
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            return true;
-        default:
-            return false;
+        // --- Using while loop ---
+        int n1 = num;
+        int sum1 = 0;
+        while (n1 > 0) {
+            int digit = n1 % 10;
+            sum1 += digit;
+            n1 = n1 / 10;
         }
-    }
+        System.out.println("Sum using while loop = " + sum1);
 
+        // --- Using do-while loop ---
+        int n2 = num;
+        int sum2 = 0;
+        do {
+            int digit = n2 % 10;
+            sum2 += digit;
+            n2 = n2 / 10;
+        } while (n2 > 0);
+        System.out.println("Sum using do-while loop = " + sum2);
+
+        // --- Using for loop ---
+        int sum3 = 0;
+        for (int n3 = num; n3 > 0; n3 = n3 / 10) {
+            int digit = n3 % 10;
+            sum3 += digit;
+        }
+        System.out.println("Sum using for loop = " + sum3);
+    }
 }
