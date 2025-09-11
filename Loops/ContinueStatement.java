a56977f218b136e7322b08b0a66cad479ac1434c
APV <42871943+vysala@users.noreply.github.com>
2025-09-11 09:24:29 -0700

    Update ContinueStatement.java

--- Loops/ContinueStatement.java
+++ Loops/ContinueStatement.java
@@ -1,1 +1,19 @@
 
+public class ContinueStatement {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++)
+		{			
+			if(i==3 || i==5 || i==7)  
+			
+			{
+				continue;
+			}
+			System.out.println(i);  
+			
+		}
+
+	}
+
+}
