424785f9ca3cf21ba05810b5c95617f1badfc204
APV <42871943+vysala@users.noreply.github.com>
2025-10-01 19:43:08 -0700

    Update EmployeeConstructorInit.java

--- Classes/EmployeeConstructorInit.java
+++ Classes/EmployeeConstructorInit.java
@@ -1,62 +1,66 @@
+
+/* Modified the employee class to add a display method that displays all the datamembers and removed the 
+ * getSalary() method
+ * Program to show default and parameterized constructor
+ */
+
 package ClassesDemo;
 
-class Employee {
-
+class Employee1 {
+// instance varaiables 
 String name;
 int id;
 double salary;
 
+//instance methods 
 void work(){
     System.out.println(name + " is working");
 }
 
+void display(){
+    System.out.println("Name= " + name + " id = " +id + " salary= " +salary);
+}
 
+public Employee1() {  // default constructor 
+}
 
-public Employee() {
-    this.name="Unknow";
+public Employee1(String name) {   //  constructor with default value assigned for id and salary
+    this.name=name;
     this.id=0;
-    this.salary=0;
+    this.salary=0.0;
 }
 
 
-
-public Employee(String name, int id, double salary) {
-    this.name = name;
+public Employee1(String name, int id, double salary) {   // parameterized constructor
+    this.name = name;  
     this.id = id;
     this.salary = salary;
 }
 
 
 
+//setter methods
 public void setName(String name) {
     this.name = name;
 }
-
-
 
 public void setId(int id) {
     this.id = id;
 }
 
 
-
 public void setSalary(double salary) {
     this.salary = salary;
 }
 
-
-
+//getter methods
 public String getName() {
     return name;
 }
 
-
-
 public int getId() {
     return id;
 }
-
-
 
 public double getSalary() {
     return salary;
@@ -67,8 +71,17 @@ public class EmployeeConstructorInit {
 
  public static void main(String[] args) {
     //obj 1
-    Employee e=new Employee();
+    Employee1 e=new Employee1("Bob",101,25499);
     System.out.println(e.getSalary());
+    e.display();
+   
+    //obj2
+    Employee1 e1=new Employee1("Alice");
+    e1.display();
+
+    //obj3
+    Employee1 e2=new Employee1(null);
+    e2.display();
     
     
  }
