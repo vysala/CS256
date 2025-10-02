677f47c36d94c9fd29b3c194d02ceac33238ffc4
APV <42871943+vysala@users.noreply.github.com>
2025-10-01 19:51:14 -0700

    Update StudentMain.java

--- Classes/StudentMain.java
+++ Classes/StudentMain.java
@@ -1,26 +1,25 @@
 package ClassesDemo;
 
-// Used the Student class blueprint created inside the StudentBP class to create student object in this class
 public class StudentMain {
+
 public static void main(String[] args) {
-       Student s1 = new Student();
-       s1.name = "Alice";
-       s1.age = 20;
-
-       Student s2 = new Student();
-       s2.name = "Bob";
-       s2.age = 22;
-
-        // Each object has its own data
-        s1.display();   // Alice is 20 years old.
-        s2.display();   // Bob is 22 years old.
     
-    Student s5=new Student();
-    s5.display();
+    Student s=new Student();
+    //1st method
 
-    Student s6=new Student("Kevin",50);
-    s6.display();
+    s.name="Alice";
+    s.age=60;
+    s.display();
+
+    Student s1=new Student();
+    s1.display();
+
+    Student s3=new Student("Bob",50);
+    s3.display();
+    s3.setAge(40);
+    s3.display();
+
 
 }
+
 }
-  
