11b045989df9698d755a9c44f18ccba613b649eb
APV <42871943+vysala@users.noreply.github.com>
2026-08-31 11:28:32 -0700

    Update FloatingPoint.java

--- ch02/FloatingPoint.java
+++ ch02/FloatingPoint.java
@@ -11,20 +11,22 @@ public class FloatingPoint {
         double pi;
         pi = 3.14159;
 
-        //Calculates and prints the fraction of an hour that 59 minutes represents.
+        //Calculates and prints the fraction of an hour that 59 minutes represents. The .0 tells Java that these are floating-point values.
         double minute3 = 59.0;
         System.out.print("Fraction of the hour that has passed: ");
-        System.out.println(minute3 / 60.0);
+        System.out.println(minute3 / 60.0);   // gives 0.983333333333
 
+        //Try running this line to see the difference in output, System.out.println(59 / 60);  because both values are integers, so Java performs integer division.
         //Assigns a fraction to a double to demonstrate floating-point precision.
 
-        double y = 1.0 / 3.0;  // correct
+        double y = 1.0 / 3.0;  // 1 ÷ 3 = 0.333333333333...  The decimal goes on forever. A computer, however, has a limited amount of space to store a number, so it stores an approximation.
 
         //Shows how floating-point arithmetic can produce rounding errors (important concept!).
-        System.out.println(0.1 * 10);
+        System.out.println(0.1 * 10);  // you expect 1.0
         System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1
                          + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
-
+        /*  Computers store numbers using bits (0s and 1s). Some decimal numbers, such as 0.1, cannot be represented exactly in binary. So Java stores a very close approximation. 
+        When we add that approximation many times, the tiny difference can become visible. */
         //Demonstrates a safer way to store balance using integers (in cents) instead of floating-point types like double.
         double balance = 123.45;  // potential rounding error
         int balance2 = 12345;     // total number of cents; this will work as lomg as the number of cents doesn't exceed the largest int.
