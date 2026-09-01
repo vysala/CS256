edb27666ffd7ea573a001b8502af42880b7082e1
APV <42871943+vysala@users.noreply.github.com>
2026-09-01 12:33:57 -0700

    Update IncrementOperator.java

--- JavaOperators/IncrementOperator.java
+++ JavaOperators/IncrementOperator.java
@@ -1,5 +1,7 @@
 package JavaOperators;
-
+/*a++ and ++a both increase a by 1. The difference is when the increment happens relative to using the value. 
+   a++ is called post increment and ++a is called pre-increment.
+*/
 public class IncrementOperator {
 
 	    //a++;    // this is post increment
@@ -14,18 +16,20 @@ public class IncrementOperator {
 		System.out.println(a); //11  */
 		
 		
-		//Case 2
+		//Case 2 : Because ++ is after a, this is post-increment. ie, use the current value first, then increment.
 		  /*int res=a++;
 		 System.out.println(res);  //10 */
 		
-		//Case 3
-		  // int res=++a;
+		//Case 3 : Here, ++ comes before a, so this is pre-increment. Increment first, then use the value.
+		  // int res=++a;   
 		   //System.out.println(res); //11 
 		
 		//Case4 - there is difference
 		System.out.println(a++);  //10 
 		System.out.println(++a);  //12  
-		
+
+
+		//Both a++ and ++a ultimately increase a by 1. The difference becomes visible when you do something like int res=a++;
 		
 		
 	}
