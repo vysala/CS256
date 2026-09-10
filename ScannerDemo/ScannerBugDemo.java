7dcfdd0239e6c634e682cb6edd1c0bbe4a90d82e
APV <42871943+vysala@users.noreply.github.com>
2026-09-10 12:09:50 -0700

    Update ScannerBugDemo.java

--- ScannerDemo/ScannerBugDemo.java
+++ ScannerDemo/ScannerBugDemo.java
@@ -27,6 +27,7 @@ public class ScannerBugDemo {
         System.out.print("What is your name? ");
         name = in.nextLine();
         System.out.printf("Hello %s, age %d\n", name, age);
+        in.close();
     }
 
 }
