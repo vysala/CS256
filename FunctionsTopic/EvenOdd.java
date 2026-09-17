c0770ff72167b39e863d844c5f42cde52c7c3efe
APV <42871943+vysala@users.noreply.github.com>
2026-09-16 17:35:13 -0700

    Update EvenOdd.java

--- FunctionsTopic/EvenOdd.java
+++ FunctionsTopic/EvenOdd.java
@@ -1,13 +1,18 @@
 package FunctionsTopic;
 
 public class EvenOdd {
-// Function to check even or odd
+// Function to check even or odd using ternary operator
+    static void checkEvenOdd(int n) {
+        System.out.println((n % 2 == 0? n + " is Even" :n + " is Odd"));
+               }
+
+   /* Try solving the same problem using conditional statements once you learn about it
     static void checkEvenOdd(int n) {
         if (n % 2 == 0)
             System.out.println(n + " is Even");
         else
             System.out.println(n + " is Odd");
-    }
+    }*/
 
     public static void main(String[] args) {
         checkEvenOdd(10);
